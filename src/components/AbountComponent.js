f99e6a914c7ee86e462e7402ed289d9e0887f7f3
Katy Pitts <kvp06933@gmail.com>
2021-06-08 17:40:43 -0500

    about component updated

--- src/components/AbountComponent.js
+++ src/components/AbountComponent.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-function About() {
+function About(){
     return(
-        <div>About</div>
-    );
-};
+       <div>
+           <h1>About Me</h1>
+           <img src="/assets/images/profile pic.png" width="10%" alt="profile pic"/>
+           <p>Hello. My name is Katherine Pitts, and I'm a full stack software developer. I got a degree in mechanical engineering and had a successful career in the biomedical industry for a few years. During this time I was exposed to software engineering in the field and realized its potential to help the community and create positive change. I left my mechanical engineering job and decided to pursue software development full time.</p>
+           <p>I graduated from Nucamp Coding Bootcamp in July 2020 as a fullstack React developer. I also added in Java to enable me to create complex applications that solve real problems. If you think our goals and mission align, I would love to work with you. </p>
+       </div>
+    )   
+   }
 
 export default About;
